441d27f3a292297b324d1042c164559d8d8ce081
aecbtt <al286276@uji.es>
2019-05-11 18:59:48 +0200

    Add files via upload

--- toopots/src/main/java/es/uji/ei1027/toopots/model/Login.java
+++ toopots/src/main/java/es/uji/ei1027/toopots/model/Login.java
@@ -3,6 +3,7 @@ package es.uji.ei1027.toopots.model;
 public class Login {
 	private String usuario;
 	private String contraseña;
+	private String rol;
 	
 	public String getUsuario() {
 		return usuario;
@@ -15,6 +16,12 @@ public class Login {
 	}
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
 	}
 	
 }
