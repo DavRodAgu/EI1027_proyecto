f57b1365db1a297d186b2c1cc11e301b25a694d4
aecbtt <al286276@uji.es>
2019-06-02 17:20:20 +0200

    Add files via upload

--- toopots/src/main/java/es/uji/ei1027/toopots/dao/TipoActividadDao.java
+++ toopots/src/main/java/es/uji/ei1027/toopots/dao/TipoActividadDao.java
@@ -1,6 +1,5 @@
 package es.uji.ei1027.toopots.dao;
 
-
 import es.uji.ei1027.toopots.model.TipoActividad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -11,51 +10,61 @@ import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository 
+@Repository
 public class TipoActividadDao {
 
-    private JdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
-    public void addTipoActividad(TipoActividad tipoActividad) {
-        jdbcTemplate.update("INSERT INTO TipoActividad VALUES(nextval('tipoactividad_idtipoactividad_seq'), ?, ?)",
-                tipoActividad.getNombre(), tipoActividad.getNivel());
-    }
+	public void addTipoActividad(TipoActividad tipoActividad) {
+		jdbcTemplate.update("INSERT INTO TipoActividad VALUES(nextval('tipoactividad_idtipoactividad_seq'), ?, ?)",
+				tipoActividad.getNombre(), tipoActividad.getNivel());
+	}
 
-    public void deleteTipoActividad(TipoActividad tipoActividad) {
-        jdbcTemplate.update("DELETE from TipoActividad where idTipoActividad=?", tipoActividad.getIdTipoActividad());
-    }
-    
-    public void deleteTipoActividad(String idTipoActividad) {
-        jdbcTemplate.update("DELETE from TipoActividad where idTipoActividad=?", Integer.valueOf(idTipoActividad));
-    }
+	public void deleteTipoActividad(TipoActividad tipoActividad) {
+		jdbcTemplate.update("DELETE from TipoActividad where idTipoActividad=?", tipoActividad.getIdTipoActividad());
+	}
 
-    public void updateTipoActividad(TipoActividad tipoActividad) {
-        jdbcTemplate.update("UPDATE tipoActividad SET nombre=?, nivel=? where idTipoActividad=?",
-        		tipoActividad.getNombre(), tipoActividad.getNivel(), tipoActividad.getIdTipoActividad());
-    }
+	public void deleteTipoActividad(String idTipoActividad) {
+		jdbcTemplate.update("DELETE from TipoActividad where idTipoActividad=?", Integer.valueOf(idTipoActividad));
+	}
 
-    public TipoActividad getTipoActividad(String idTipoActividad) {
-        try {
-            return jdbcTemplate.queryForObject("SELECT * from TipoActividad WHERE idTipoActividad=?",
-                    new TipoActividadRowMapper(), Integer.valueOf(idTipoActividad));
-        }
-        catch(EmptyResultDataAccessException e) {
-            return null;
-        }
-    }
+	public void updateTipoActividad(TipoActividad tipoActividad) {
+		jdbcTemplate.update("UPDATE tipoActividad SET nombre=?, nivel=? where idTipoActividad=?",
+				tipoActividad.getNombre(), tipoActividad.getNivel(), tipoActividad.getIdTipoActividad());
+	}
 
-    public List<TipoActividad> getTipoActividades() {
-        try {
-            return jdbcTemplate.query("SELECT * from TipoActividad",
-                    new TipoActividadRowMapper());
-        }
-        catch(EmptyResultDataAccessException e) {
-            return new ArrayList<TipoActividad>();
-        }
-    }
+	public TipoActividad getTipoActividad(String idTipoActividad) {
+		try {
+			return jdbcTemplate.queryForObject("SELECT * from TipoActividad WHERE idTipoActividad=?",
+					new TipoActividadRowMapper(), Integer.valueOf(idTipoActividad));
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+
+	public List<TipoActividad> getTipoActividades() {
+		try {
+			return jdbcTemplate.query("SELECT * from TipoActividad", new TipoActividadRowMapper());
+		} catch (EmptyResultDataAccessException e) {
+			return new ArrayList<TipoActividad>();
+		}
+	}
+
+	public List<TipoActividad> getTipoActividadesInstructor(String idInstructor) {
+		try {
+			return jdbcTemplate.query(
+					"select tipoactividad.* FROM tipoactividad"
+					+ " INNER JOIN acredita ON tipoactividad.idtipoactividad=acredita.idtipoactividad"
+					+ " INNER JOIN acreditacion ON acredita.idacreditacion=acreditacion.idacreditacion"
+					+ " WHERE acreditacion.idinstructor=?;",
+					new TipoActividadRowMapper(), idInstructor);
+		} catch (EmptyResultDataAccessException e) {
+			return new ArrayList<TipoActividad>();
+		}
+	}
 }
