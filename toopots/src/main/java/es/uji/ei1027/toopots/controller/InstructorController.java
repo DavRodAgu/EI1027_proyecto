6c36bd6240165f050116da9d6b3cba300c647165
aecbtt <al286276@uji.es>
2019-06-17 19:30:55 +0200

    update con foto

--- toopots/src/main/java/es/uji/ei1027/toopots/controller/InstructorController.java
+++ toopots/src/main/java/es/uji/ei1027/toopots/controller/InstructorController.java
@@ -1,11 +1,15 @@
 package es.uji.ei1027.toopots.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import es.uji.ei1027.toopots.dao.InstructorDao;
@@ -111,32 +116,80 @@ public class InstructorController {
 		return "instructor/perfil";
 	}
 
+	public static File convert(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
+	}
+
 	@RequestMapping(value = "/perfil/{idInstructor}", method = RequestMethod.POST)
-	public String updatePerfilInstructor(HttpSession session, Model model, @PathVariable String idInstructor,
-			@ModelAttribute("instructor") Instructor instructor, BindingResult bindingResult,
-			RedirectAttributes redirectAttributes) {
+	public String updatePerfilInstructor(HttpSession session, Model model, @RequestParam("afoto") MultipartFile foto,
+			@PathVariable String idInstructor, @RequestParam("nombre") String nombre,
+			@RequestParam("estado") String estado, @RequestParam("email") String email,
+			@RequestParam("iban") String iban, @ModelAttribute("instructor") Instructor instructor
+	/* BindingResult bindingResult, RedirectAttributes redirectAttributes */) throws IOException {
 		if (session.getAttribute("user") == null) {
 			model.addAttribute("user", new Login());
 			session.setAttribute("nextUrl", "instructor/perfil");
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!usuario.getRol().equals("instructor")) {
-			return "error/error";
-		}
-		
-		if (bindingResult.hasErrors()) {
-			redirectAttributes.addFlashAttribute("message", "Error al actualizar su perfil");
-			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
-			return "cliente/perfil";
+			model.addAttribute("user", new Login());
+			session.setAttribute("nextUrl", "instructor/perfil");
+			return "login";
 		}
 
+		/*
+		 * if (bindingResult.hasErrors()) {
+		 * redirectAttributes.addFlashAttribute("message",
+		 * "Error al actualizar su perfil");
+		 * redirectAttributes.addFlashAttribute("alertClass", "alert-danger"); return
+		 * "instructor/perfil"; }
+		 */
+
+		/*
+		 * Se convierte el MultipartFile a File, se escribe el archivo en su ruta
+		 * automática, se toma la ruta absoluta del archivo, se corta el nombre del
+		 * archivo para conseguir la ruta absoluta del directorio, se crea un File en el
+		 * directorio media
+		 */
+		File archivo = convert(foto);
+		archivo.createNewFile();
+		String directorio = archivo.getAbsolutePath();
+		directorio = directorio.substring(0, directorio.length() - archivo.getPath().length() - 1);
+		String nombreFoto = archivo.getName();
+
+		File destination = new File(directorio + "/src/main/resources/static/media/" + nombreFoto);
+
+		/* Copiar fichero a directorio destino */
+		try {
+			FileUtils.copyFile(archivo, destination);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/*
+		 * Se escribe el fichero en el directorio media y se borra el archivo del
+		 * directorio por defecto
+		 */
+		destination.createNewFile();
+		archivo.delete();
+
 		// TODO: Modificar este código en un futuro
-		instructor.setFoto("/foto");
-		instructorDao.updateInstructor(instructor);
-		redirectAttributes.addFlashAttribute("message", "Cambios guardados");
-		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		Instructor ins = instructorDao.getInstructor(idInstructor);
+		ins.setNombre(nombre);
+		ins.setEstado(estado);
+		ins.setEmail(email);
+		ins.setIban(iban);
+		ins.setFoto(destination.getName());
+		instructorDao.updateInstructor(ins);
+		//redirectAttributes.addFlashAttribute("message", "Cambios guardados");
+		//redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 		return "redirect:../perfil";
 	}
 
