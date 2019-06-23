ad8e36fb603548e1a2c625b2758bc0282d8cc29a
aecbtt <al286276@uji.es>
2019-06-23 18:36:54 +0200

    Add files via upload

--- toopots/src/main/java/es/uji/ei1027/toopots/controller/ClienteController.java
+++ toopots/src/main/java/es/uji/ei1027/toopots/controller/ClienteController.java
@@ -42,18 +42,18 @@ import es.uji.ei1027.toopots.model.Reserva;
 import es.uji.ei1027.toopots.model.ImagenPromocional;
 import es.uji.ei1027.toopots.services.ClienteService;
 
-class ClienteValidator implements Validator { 
+class ClienteValidator implements Validator {
 	@Override
-	public boolean supports(Class<?> cls) { 
+	public boolean supports(Class<?> cls) {
 		return Cliente.class.isAssignableFrom(cls);
 	}
-	@Override 
+
+	@Override
 	public void validate(Object obj, Errors errors) {
-		Cliente cliente = (Cliente)obj;
+		Cliente cliente = (Cliente) obj;
 		// TODO: Añadir comprobaciones
 	}
 }
-
 
 @Controller
 @RequestMapping("/cliente")
@@ -68,7 +68,7 @@ public class ClienteController {
 	private ImagenPromocionalDao imagenPromocionalDao;
 	private ReservaDao reservaDao;
 	private ComentarioDao comentarioDao;
-	
+
 	@Autowired
 	public void setClienteDao(ClienteDao clienteDao) {
 		this.clienteDao = clienteDao;
@@ -78,17 +78,17 @@ public class ClienteController {
 	public void setActividadDao(ActividadDao actividadDao) {
 		this.actividadDao = actividadDao;
 	}
-	
+
 	@Autowired
 	public void setClienteService(ClienteService clienteService) {
 		this.clienteService = clienteService;
 	}
-	
+
 	@Autowired
 	public void setLoginDao(LoginDao loginDao) {
 		this.loginDao = loginDao;
 	}
-	
+
 	@Autowired
 	public void setTipoActividadDao(TipoActividadDao tipoActividadDao) {
 		this.tipoActividadDao = tipoActividadDao;
@@ -103,18 +103,17 @@ public class ClienteController {
 	public void setImagenPromocionalDao(ImagenPromocionalDao imagenPromocionalDao) {
 		this.imagenPromocionalDao = imagenPromocionalDao;
 	}
-	
+
 	@Autowired
 	public void setReservaDao(ReservaDao reservaDao) {
 		this.reservaDao = reservaDao;
 	}
-	
+
 	@Autowired
 	public void setComentarioDao(ComentarioDao comentarioDao) {
 		this.comentarioDao = comentarioDao;
 	}
-	
-	
+
 	@RequestMapping("/actividades")
 	public String listActividades(HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -122,12 +121,12 @@ public class ClienteController {
 			session.setAttribute("nextUrl", "cliente/actividades");
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!(usuario.getRol().equals("cliente"))) {
 			return "error/error";
 		}
-		
+
 		HashMap<Integer, String> imagenes = new HashMap<Integer, String>();
 		for (ImagenPromocional imagen : imagenPromocionalDao.getImagenesPromocionales()) {
 			imagenes.put(imagen.getIdActividad(), imagen.getImagen());
@@ -138,7 +137,7 @@ public class ClienteController {
 		model.addAttribute("imagenes", imagenes);
 		return "cliente/actividades";
 	}
-	
+
 	@RequestMapping("/actividades/{preferencias}")
 	public String listActividadesPref(@PathVariable boolean preferencias, HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -146,12 +145,12 @@ public class ClienteController {
 			session.setAttribute("nextUrl", "cliente/actividades");
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!usuario.getRol().equals("cliente")) {
 			return "error/error";
 		}
-		
+
 		HashMap<Integer, String> imagenes = new HashMap<Integer, String>();
 		for (ImagenPromocional imagen : imagenPromocionalDao.getImagenesPromocionales()) {
 			imagenes.put(imagen.getIdActividad(), imagen.getImagen());
@@ -161,7 +160,7 @@ public class ClienteController {
 		model.addAttribute("imagenes", imagenes);
 		return "cliente/actividades";
 	}
-	
+
 	@RequestMapping("/actividad/{idActividad}")
 	public String infoActividad(@PathVariable String idActividad, HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -169,18 +168,18 @@ public class ClienteController {
 			session.setAttribute("nextUrl", "cliente/actividad/" + idActividad);
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!usuario.getRol().equals("cliente")) {
 			return "error/error";
 		}
-		
+
 		model.addAttribute("actividad", clienteService.getActividad(usuario.getUsuario(), idActividad));
 		model.addAttribute("imagen", imagenPromocionalDao.getImagen(idActividad));
 		model.addAttribute("comentarios", clienteService.getComentarios(idActividad));
 		return "cliente/actividad";
 	}
-	
+
 	@RequestMapping(value = "/anadirReserva", method = RequestMethod.POST)
 	public String processAñadirSubmit(Model model, HttpSession session, @ModelAttribute("reserva") Reserva reserva,
 			@RequestParam(name = "idActividad") String idActividad, @RequestParam(name = "nPersonas") int nPersonas,
@@ -206,41 +205,43 @@ public class ClienteController {
 		reserva.setFecha(ts);
 		reserva.setPrecioPorPersona(actividad.getPrecio());
 		reservaDao.addReserva(reserva);
-		
-		redirectAttributes.addFlashAttribute("message", "Se ha reservado la actividad \"" + actividad.getNombre() + "\"");
+
+		redirectAttributes.addFlashAttribute("message",
+				"Se ha reservado la actividad \"" + actividad.getNombre() + "\"");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
-		
+
 		return "redirect:../cliente/actividad/" + idActividad;
 	}
-	
+
 	@RequestMapping(value = "/actividad/{idActividad}/comentario", method = RequestMethod.POST)
-	public String añadirComentario(HttpSession session, Model model, @PathVariable String idActividad, @RequestParam("valoracion") int valoracion, 
-			@RequestParam("comment") String comment, RedirectAttributes redirectAttributes) {
+	public String añadirComentario(HttpSession session, Model model, @PathVariable String idActividad,
+			@RequestParam("valoracion") int valoracion, @RequestParam("comment") String comment,
+			RedirectAttributes redirectAttributes) {
 		if (session.getAttribute("user") == null) {
 			model.addAttribute("user", new Login());
 			session.setAttribute("nextUrl", "cliente/actividad/" + idActividad);
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!usuario.getRol().equals("cliente")) {
 			return "error/error";
 		}
-		
+
 		Comentario comentario = new Comentario();
 		comentario.setIdActividad(Integer.parseInt(idActividad));
 		comentario.setValoracion(valoracion);
 		comentario.setComentario(comment);
 		comentario.setIdCliente(usuario.getUsuario());
-		
+
 		comentarioDao.addComentario(comentario);
-		
+
 		redirectAttributes.addFlashAttribute("message", "Comentario añadido");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
-		
+
 		return "redirect:/cliente/actividad/" + idActividad;
 	}
-	
+
 	@RequestMapping("/reservas")
 	public String listReservas(HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -255,7 +256,7 @@ public class ClienteController {
 		model.addAttribute("reservas", clienteService.getReservaByClient(usuario.getUsuario()));
 		return "cliente/reservas";
 	}
-	
+
 	@RequestMapping("/perfil")
 	public String perfilUsuario(HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -271,41 +272,45 @@ public class ClienteController {
 		model.addAttribute("tipos", tipoActividadDao.getTipoActividades());
 		List<Prefiere> prefiere = prefiereDao.getPreferenciasCliente(usuario.getUsuario());
 		List<Integer> ids = new ArrayList<Integer>();
-		for(Prefiere prf : prefiere) {
+		for (Prefiere prf : prefiere) {
 			ids.add(prf.getIdTipoActividad());
 		}
 		model.addAttribute("preferencias", ids);
 		return "cliente/perfil";
 	}
-	
+
 	@RequestMapping(value = "/perfil", method = RequestMethod.POST)
-	public String prefiereUpdate(@RequestParam("prefiere") List<Integer> new_preferencias, HttpSession session,
-			RedirectAttributes redirectAttributes) {
+	public String prefiereUpdate(@RequestParam(value = "prefiere", required = false) List<Integer> new_preferencias,
+			HttpSession session, RedirectAttributes redirectAttributes) {
 		// TODO: Código de mierda, pero funciona
 		Login usuario = (Login) session.getAttribute("user");
 		List<Prefiere> old_preferencias = prefiereDao.getPreferenciasCliente(usuario.getUsuario());
-		if (old_preferencias.size() < new_preferencias.size()) {
+		if (new_preferencias == null) {
+			for (int i = 0; i < old_preferencias.size(); i++) {
+				prefiereDao.deletePrefiere(old_preferencias.get(i));
+			}
+		}else if (old_preferencias.size() < new_preferencias.size()) {
 			// Se han añadido preferencias
 			Prefiere pref = null;
-			for(int i = 0; i < new_preferencias.size(); i++) {
-				if(prefiereDao.getPrefiere(new_preferencias.get(i).intValue(), usuario.getUsuario()) == null) {
+			for (int i = 0; i < new_preferencias.size(); i++) {
+				if (prefiereDao.getPrefiere(new_preferencias.get(i).intValue(), usuario.getUsuario()) == null) {
 					pref = new Prefiere();
 					pref.setIdTipoActividad(new_preferencias.get(i).intValue());
 					pref.setIdCliente(usuario.getUsuario());
 					prefiereDao.addPrefiere(pref);
 				}
-			}			
+			}
 		} else if (old_preferencias.size() > new_preferencias.size()) {
 			// Se han borrado preferencias
-			for(int i = 0; i < old_preferencias.size(); i++) {
-				if(!(new_preferencias.contains(old_preferencias.get(i).getIdTipoActividad()))) {
+			for (int i = 0; i < old_preferencias.size(); i++) {
+				if (!(new_preferencias.contains(old_preferencias.get(i).getIdTipoActividad()))) {
 					prefiereDao.deletePrefiere(old_preferencias.get(i));
 				}
 			}
 		}
 		return "redirect:perfil";
 	}
-	
+
 	@RequestMapping(value = "/perfil/{idCliente}", method = RequestMethod.POST)
 	public String processPerfilUpdate(@PathVariable String idCliente, @ModelAttribute("cliente") Cliente cliente,
 			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
@@ -317,7 +322,7 @@ public class ClienteController {
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 		return "redirect:../perfil";
 	}
-	
+
 	@RequestMapping(value = "/perfil/password")
 	public String processChangePassword(HttpSession session, Model model) {
 		if (session.getAttribute("user") == null) {
@@ -325,15 +330,15 @@ public class ClienteController {
 			session.setAttribute("nextUrl", "cliente/perfil");
 			return "login";
 		}
-		
+
 		Login usuario = (Login) session.getAttribute("user");
 		if (!usuario.getRol().equals("cliente")) {
 			return "error/error";
 		}
-		
+
 		return "cliente/perfil/password";
 	}
-	
+
 	@RequestMapping(value = "/perfil/password", method = RequestMethod.POST)
 	public String processUpdatePassword(HttpSession session, @RequestParam("password") String password,
 			@RequestParam("oldpassword") String oldPassword, @RequestParam("reppassword") String repPassword,
@@ -350,7 +355,7 @@ public class ClienteController {
 			return "redirect:../perfil/password";
 		}
 		// TODO: Añadir alguna comprobación más?
-		
+
 		user.setContraseña(password);
 		if (loginDao.updatePassword(user)) {
 			redirectAttributes.addFlashAttribute("message", "Contraseña cambiada");
@@ -361,49 +366,51 @@ public class ClienteController {
 
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String processAddSubmit(@ModelAttribute("cliente") Cliente cliente, BindingResult bindingResult,
-			@RequestParam("contraseña-cliente") String passwd, @RequestParam("contraseña-cliente-rep") String passwdRep, 
+			@RequestParam("contraseña-cliente") String passwd, @RequestParam("contraseña-cliente-rep") String passwdRep,
 			RedirectAttributes redirectAttributes) {
-			
+
 		// Crear objeto Login
 		Login login = new Login();
 		login.setUsuario(cliente.getIdCliente());
 		login.setContraseña(passwd);
 		login.setRol("cliente");
-		
+
 		// Crear el objeto validator
-		ClienteValidator userValidator = new ClienteValidator(); 
+		ClienteValidator userValidator = new ClienteValidator();
 		userValidator.validate(cliente, bindingResult);
-		
+
 		if (cliente.getIdCliente().length() != 9 || Character.isLetter(cliente.getIdCliente().charAt(8)) == false) {
-			redirectAttributes.addFlashAttribute("errorDNI", "Formato de DNI incorrecto, recuerda que debes introducir 8 dígitos y 1 letra");
+			redirectAttributes.addFlashAttribute("errorDNI",
+					"Formato de DNI incorrecto, recuerda que debes introducir 8 dígitos y 1 letra");
 			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
 			return "redirect:/register";
 		}
-		
+
 		List<String> DNIs = clienteDao.getClientes().stream().map(Cliente::getIdCliente).collect(Collectors.toList());
-		
+
 		if (DNIs.contains(cliente.getIdCliente())) {
 			redirectAttributes.addFlashAttribute("errorDNI", "El DNI introducido ya se encuentra registrado");
 			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
 			return "redirect:/register";
 		}
-		
+
 		if (!(passwd.equals(passwdRep))) {
 			redirectAttributes.addFlashAttribute("errorPasswd", "Las contraseñas no coinciden");
 			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
 			return "redirect:/register";
-			//return "registro";
+			// return "registro";
 		}
-		
+
 		if (bindingResult.hasErrors()) {
 			return "registro";
-		} 
-		
+		}
+
 		// Añadir la información en la base de datos
 		clienteDao.addCliente(cliente);
 		loginDao.addLogin(login);
-		
-		redirectAttributes.addFlashAttribute("register", "Su cuenta ha sido creada. Inicie sesión para empezar a reservar sus actividades.");
+
+		redirectAttributes.addFlashAttribute("register",
+				"Su cuenta ha sido creada. Inicie sesión para empezar a reservar sus actividades.");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
 		return "redirect:/login";
 	}
